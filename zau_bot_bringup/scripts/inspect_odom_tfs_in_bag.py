45b4b8acf275723fea3ce49e4adc03a742fb2980
Miguel Oliveira <m.riem.oliveira@gmail.com>
2023-05-21 02:11:14 +0100

    A script to inspect the bag. Correcting the recorded bag file #21

--- zau_bot_bringup/scripts/inspect_odom_tfs_in_bag.py
+++ zau_bot_bringup/scripts/inspect_odom_tfs_in_bag.py
@@ -45,21 +45,33 @@ if __name__ == "__main__":
     for topic, msg, stamp in bag.read_messages():
         mission_time = (stamp - initial_stamp)
 
-        if mission_time.to_sec() > 10: # just for testing fast, analyze messages only until 10 secs mission time.
+        if mission_time.to_sec() > 30: # just for testing fast, analyze messages only until 10 secs mission time.
             break
         
         if topic == '/tf':
 
             for transform_idx, transform in enumerate(msg.transforms): # iterate all transforms
                 # ros transform msg: http://docs.ros.org/en/noetic/api/tf/html/msg/tfMessage.html
-                if transform.header.frame_id == 'zau/map_rn0' and transform.child_frame_id =='zau/odom': # the specific transform we want to change
-                    # print('Msg on topic ' + topic + ' stamp=' + str(stamp))
-                    # print('This is a tf message')
-                    # print('Transform idx ' + str(transform_idx) + ' should be changed')
-
+                if transform.header.frame_id == 'zau/map_rn0' and transform.child_frame_id =='zau/odom': 
                     # Set a new transform value for rotation
                     print('Got transform in msg on topic /tf with parent ' + transform.header.frame_id + ' and child ' + transform.child_frame_id  + ' at time ' + str((stamp-initial_stamp).to_sec()))
                     print('transform.header.stamp = ' + str((transform.header.stamp-initial_stamp).to_sec()))
+
+                    from sensor_msgs.msg import JointState
+                    j = JointState()
+                    j.header.stamp = transform.header.stamp
+                    bag_out.write('odom_tf_msg', j, stamp)
+
+                if transform.header.frame_id == 'zau/base_link' and transform.child_frame_id =='cam_2_link': 
+                    # Set a new transform value for rotation
+                    print('Got transform in msg on topic /tf with parent ' + transform.header.frame_id + ' and child ' + transform.child_frame_id  + ' at time ' + str((stamp-initial_stamp).to_sec()))
+                    print('transform.header.stamp = ' + str((transform.header.stamp-initial_stamp).to_sec()))
+
+                    from sensor_msgs.msg import JointState
+                    j = JointState()
+                    j.header.stamp = transform.header.stamp
+                    bag_out.write('cam_2_tf_msg', j, stamp)
+
 
             # msg.transforms.extend(static_tfs) # extend static tfs to tf topic
 
