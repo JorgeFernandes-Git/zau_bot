dd099bdda1496410713ebd6b9df2dd2db711cdc9
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-05-22 21:52:01 +0100

    Add pub static and tf

--- zau_bot_bringup/scripts/static_dynamic_pub.py
+++ zau_bot_bringup/scripts/static_dynamic_pub.py
@@ -8,7 +8,10 @@ import math
 if __name__ == '__main__':
     
     rospy.init_node('tf2_broadcaster')
-    br = tf2_ros.TransformBroadcaster()
+    
+    br_in_tf = tf2_ros.TransformBroadcaster()
+    br_in_static_tf = tf2_ros.StaticTransformBroadcaster()
+    
     d = geometry_msgs.msg.TransformStamped()
     s = geometry_msgs.msg.TransformStamped()
 
@@ -18,31 +21,31 @@ if __name__ == '__main__':
     s.header.frame_id = "parent"
     s.child_frame_id = "static_child"
 
+    s.header.stamp = rospy.Time.now()
+    s.transform.translation.x = 0.0
+    s.transform.translation.y = 2.0
+    s.transform.translation.z = 0.0
+    s.transform.rotation.x = 0.0
+    s.transform.rotation.y = 0.0
+    s.transform.rotation.z = 0.0
+    s.transform.rotation.w = 1.0
+    br_in_static_tf.sendTransform(s)
+    
     rate = rospy.Rate(10.0)
     
     while not rospy.is_shutdown():
         x = rospy.Time.now().to_sec() * math.pi
 
         d.header.stamp = rospy.Time.now()
-        d.transform.translation.x = 10 * math.sin(x)
-        d.transform.translation.y = 10 * math.cos(x)
-        d.transform.translation.z = 0.0
+        s.transform.translation.x = 2.0
+        s.transform.translation.y = 0.0
+        s.transform.translation.z = 0.0
         d.transform.rotation.x = 0.0
         d.transform.rotation.y = 0.0
         d.transform.rotation.z = 0.0
         d.transform.rotation.w = 1.0
-         
-        s.header.stamp = rospy.Time.now()
-        s.transform.translation.x = 0.0
-        s.transform.translation.y = 2.0
-        s.transform.translation.z = 0.0
-        s.transform.rotation.x = 0.0
-        s.transform.rotation.y = 0.0
-        s.transform.rotation.z = 0.0
-        s.transform.rotation.w = 1.0
 
-        br.sendTransform(d)
-        br.sendTransform(s)
+        br_in_tf.sendTransform(d)
         
         rate.sleep()
         print('Publish...')
